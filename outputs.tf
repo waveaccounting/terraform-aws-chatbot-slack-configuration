d35f7530242be980f46e25765d73d240cb12e68a
Michael Warkentin <mwarkentin@gmail.com>
2020-07-08 22:57:52 -0400

    add configration_arn output

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,4 @@
-
+output "configuration_arn" {
+  description = "The ARN of the Chatbot Slack configuration"
+  value       = aws_cloudformation_stack.chatbot_slack_configuration.outputs.ConfigurationArn
+}
